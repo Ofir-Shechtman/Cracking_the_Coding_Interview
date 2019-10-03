1e1f52cabd40e102516cd771d27727880c399a6d
ofir511996 <ofir511996@gmail.com>
2019-10-03 16:14:39 +0300

    Q 1.1-1.5

--- Python Solutions/Arrays & Strings.py
+++ Python Solutions/Arrays & Strings.py
@@ -2,4 +2,20 @@
 #Arrays & Strings
 
 #1.1
-is_Unique = lambda string: len(set(string)) == len(string)
+is_unique = lambda string: len(set(string)) == len(string)
+
+#1.2
+from collections import Counter
+check_permutation = lambda s1, s2: Counter(s1)==Counter(s2)
+
+#1.3
+URLify = lambda s: '%20'.join(s.split(' '))
+URLify = lambda s: s.rstrip().replace(' ', '%20')
+
+#1.4
+mod2 = lambda x: x%2
+def palindrome_permutation(string):
+    strip=string.lower().replace(' ', '')
+    letter_sum= Counter(strip)
+    total_list= map(mod2,letter_sum.values())
+    return sum(total_list) <=1
