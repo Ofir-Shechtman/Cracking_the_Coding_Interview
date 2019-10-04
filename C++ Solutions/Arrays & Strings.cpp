dabe6582f81a0571503fad167cdee7a953c17e5b
ofir511996 <ofir511996@gmail.com>
2019-10-04 16:24:04 +0300

    finished

--- C++ Solutions/Arrays & Strings.cpp
+++ C++ Solutions/Arrays & Strings.cpp
@@ -81,6 +81,115 @@ bool OneAway(const string& s1, const string& s2){
     return true;
 }
 
+//Q1.6
+unsigned int GetCompressLength(const string& org){
+    unsigned int new_len=org.size();
+    bool second=false;
+    char prev=0;
+    for(const char c: org){
+        if(c==prev) {
+            if (second)
+                --new_len;
+            second = true;
+        }
+        else{
+            second=false;
+        }
+        prev=c;
+    }
+    return new_len;
+}
+
+char* WriteCompress(char* cur, char letter, int counter){
+    *cur++=letter;
+    if(counter==2){
+        *cur++=letter;
+    }
+    else if(counter>2){
+        *cur++=counter+'0';
+    }
+    return cur;
+
+}
+string StringCompression(const string& org){
+    unsigned int new_len=GetCompressLength(org);
+    if(new_len==org.size()) return org;
+    char output[new_len+1];
+    char* cur=output;
+    char prev=*org.begin();
+    int counter=0;
+    for(char i : org){
+        if(i == prev){
+            counter++;
+        }
+        else{
+           cur=WriteCompress(cur, prev, counter);
+           counter=0;
+        }
+        prev=i;
+    }
+    cur=WriteCompress(cur, prev, counter);
+    *cur=0;
+    return output;
+}
+
+//Q1.7
+void swap4(int& a, int& b, int& c, int& d){
+    int temp=a;
+    a=b;
+    b=c;
+    c=d;
+    d=temp;
+}
+const int N=4;
+void RotateMatrix(int matrix[N][N]){
+    for(int i=0; i<N/2; ++i){
+        for(int j=0; j<N/2; ++j){
+            swap4(matrix[i][j], matrix[j][N-i-1], matrix[N-i-1][N-j-1], matrix[N-j-1][i]);
+        }
+    }
+}
+
+//Q1.8
+const int M=5;
+//const int N=4;
+void ZeroMatrix(int matrix[M][N]){
+    bool row[M]={false};
+    bool col[N]={false};
+    for(int i=0;i<M;++i){
+        for(int j=0;j<N;++j){
+            if(matrix[i][j]==0){
+                row[i]=true;
+                col[j]=true;
+            }
+        }
+    }
+    for(int i=0;i<M;++i) {
+        for (int j = 0; j < N; ++j) {
+            if (row[i] || col[j])
+                matrix[i][j] = 0;
+        }
+    }
+}
+
+//Q1.9
+bool isSubstring(const string& s1, const string& s2){
+    for(int i=0; i<=s2.size()-s1.size(); ++i){
+        string t=s2.substr(i, s1.size());
+        if(s2.substr(i, s1.size())==s1)
+            return true;
+    }
+    return false;
+}
+
+bool StringRotation(const string& s1, const string& s2){
+    for(int i=0; i<s2.size(); ++i){
+        if(isSubstring(s2.substr(0, i), s1) && isSubstring(s2.substr(i, s2.size()), s1))
+            return true;
+    }
+    return false;
+}
+
 
 int main() {
     //cout << isUnique("Hello, World!") << endl;
@@ -93,9 +202,17 @@ int main() {
     //URLify(input);
     //cout << input << endl;
     //cout << PalindromePermutation("Tact Coa") << endl;
-    cout << OneAway("pale", "ple") << endl;
-    cout << OneAway("pales", "pale") << endl;
-    cout << OneAway("pale", "bale") << endl;
-    cout << OneAway("pale", "bake") << endl;
+    //cout << OneAway("pale", "ple") << endl;
+    //cout << OneAway("pales", "pale") << endl;
+    //cout << OneAway("pale", "bale") << endl;
+    //cout << OneAway("pale", "bake") << endl;
+    //cout << StringCompression("aaaabbbbcddaaaa") << endl;
+    //int m1[4][4] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    //RotateMatrix(m1);
+    //cout << m1 << endl;
+    //int m2[5][4] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+    //ZeroMatrix(m2);
+    cout<< isSubstring("erbottle", "waterbottle") << endl;
+    cout<< StringRotation("waterbottle", "erbottlewat") << endl;
     return 0;
 }
